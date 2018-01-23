2e3db7e7d6cb945703741eafbff2ac05bd69395d
codebyomar <omarmohammed580@gmail.com>
2018-01-23 21:39:23 +0100

    game play route created

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import signIn from '@/components/views/sign-in/sign-in'
 import newPlayer from '@/components/views/createUsername/create-username'
+import gamePlay from "@/componenets/views/gamePlay/game-play"
 
 Vue.use(Router)
 
@@ -18,6 +19,12 @@ export default new Router({
       path: '/newplayer',
       name: 'newplayer',
       component: newPlayer
+    },
+
+    {
+      path: '/gameplay',
+      name: 'game-play',
+      component: gamePlay
     }
   ]
 })
