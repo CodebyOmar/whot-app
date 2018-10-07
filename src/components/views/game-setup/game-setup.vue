8302380c9719ee897c492f87aa74658bd993951e
Ikechi Michael <mykehell123@gmail.com>
2018-10-07 13:29:28 +0100

    feat: game-setup component

---
+++ src/components/views/game-setup/game-setup.vue
@@ -0,0 +1,14 @@
+<template src="./game-setup.html">
+
+</template>
+
+<script>
+  import appLayout from "@/components/layout/app-layout"
+
+  export default {
+    name: 'gameSetup',
+    components:{
+      appLayout
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import homeComponent from '@/components/views/home/home'
-import newPlayer from '@/components/views/createUsername/create-username'
+import gameSetupComponent from '@/components/views/game-setup/game-setup'
 import gamePlay from "@/components/views/gamePlay/game-play"
 
 Vue.use(Router)
@@ -15,11 +15,10 @@ export default new Router({
       component: homeComponent
     },
     {
-      path: '/newplayer',
-      name: 'newplayer',
-      component: newPlayer
+      path: '/setup',
+      name: 'game-setup',
+      component: gameSetupComponent
     },
-
     {
       path: '/gameplay',
       name: 'game-play',
